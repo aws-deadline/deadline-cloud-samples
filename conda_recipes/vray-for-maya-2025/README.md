eb8b1e9713f4d33a1cf18a5aefb78a2882e3b084
Alex Tran <167144297+AlexTranAmz@users.noreply.github.com>
2024-12-06 13:43:06 -0800

    feat: add V-Ray for Maya conda package
    Signed-off-by: Alex Tran <167144297+AlexTranAmz@users.noreply.github.com>

---
+++ conda_recipes/vray-for-maya-2025/README.md
@@ -0,0 +1,22 @@
+# V-Ray conda build recipe
+Use this recipe to create a V-Ray for Maya conda package to use with AWS Deadline Cloud. Conda packages let you customize the software you can use with your Deadline Cloud deployment. Read more about how to host a conda channel for these custom conda packages below.
+
+## Download the archive file
+- Download the `vray_adv_62002_maya2025_rhel8` full download file from [Chaos](https://download.chaos.com/downloads/23688/vray-maya-2025-62002-adv)\
+**_NOTE:_** Need to have a Chaos account to access the link
+
+## Build Conda Package
+- place it in the `conda_recipes/archive_files` directory in your git clone of the
+[deadline-cloud-samples](https://github.com/aws-deadline/deadline-cloud-samples) repository for
+submitting package build jobs.
+
+- Follow this [README](https://github.com/aws-deadline/deadline-cloud-samples/blob/mainline/conda_recipes/README.md) to submit a job building conda package.
+
+## Requirement
+To use V-Ray for Maya, we need to build:
+- V-Ray for Maya 2025 conda package by running: `./submit-package-job vray-for-maya-2025`
+- Maya 2025 conda package by running: `./submit-package-job maya-2025`
+- (_Optional - build this for Maya adaptor_) Maya adaptor conda package by running:
+    - [_Prerequisite_]Deadline Cloud package: `./submit-package-job deadline`
+    - [_Prerequisite_]OpenJD runtime adaptor package:`./submit-package-job openjd-adaptor-runtime`
+    - Maya adaptor package:`./submit-package-job maya-openjd`
---
+++ conda_recipes/vray-for-maya-2025/recipe/build.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+set -xeuo pipefail
+
+MAYA_VERSION=2025
+
+# Run the installer extracting to a temporary directory
+ls -l $SRC_DIR
+chmod u+x $SRC_DIR/vray*
+mkdir "$SRC_DIR/extracted"
+$SRC_DIR/vray* -unpackInstall $SRC_DIR/extracted
+
+cd $PREFIX
+
+# Copy the vray relocatable (aka portable) installation from the temporary directory to the prefix
+mkdir -p "$PREFIX/usr/autodesk"
+MAYA_VRAY_ROOT="usr/autodesk/maya-vray-$MAYA_VERSION"
+MAYA_VRAY_MODULE_PATH="$MAYA_VRAY_ROOT/maya_root/modules"
+MAYA_VRAY_VRAY_ROOT="$MAYA_VRAY_ROOT/vray"
+MAYA_VRAY_MAYA_ROOT="$MAYA_VRAY_ROOT/maya_vray"
+cp -r "$SRC_DIR/extracted/" "$PREFIX/$MAYA_VRAY_ROOT"
+
+# Remove the samples, they're not needed on the farm
+rm -rf $PREFIX/$MAYA_VRAY_VRAY_ROOT/samples
+# Remove the docs, they're not needed on the farm
+rm -rf $PREFIX/$MAYA_VRAY_VRAY_ROOT/docs
+
+# Create symlinks
+mkdir -p $PREFIX/bin
+ln -r -s $PREFIX/$MAYA_VRAY_MAYA_ROOT/bin/vray $PREFIX/bin/vray
+ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/bump2gloss $PREFIX/bin/bump2gloss
+ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/imapviewer $PREFIX/bin/imapviewer
+ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/img2tiledexr $PREFIX/bin/img2tiledexr
+ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/ply2vrmesh $PREFIX/bin/ply2vrmesh
+ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/vdenoise $PREFIX/bin/vdenoise
+ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/vraydr_check $PREFIX/bin/vraydr_check
+ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/vrimg2exr $PREFIX/bin/vrimg2exr
+ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/vrmesh_viewer $PREFIX/bin/vrmesh_viewer
+ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/vrstconvert $PREFIX/bin/vrstconvert
+
+# Install dependencies not available on Deadline Cloud service-managed fleets
+mkdir -p $SRC_DIR/download
+cd $SRC_DIR/download
+dnf download --resolve -y xcb-util-image xcb-util-renderutil
+
+for rpm_file in $(realpath $SRC_DIR/download/*.rpm); do
+    rpm2cpio "$rpm_file" | cpio -idm
+done
+
+# Copy .so's to the V-Ray installation
+find . -iname "*.so.*" -exec cp -P -r {} "$PREFIX/$MAYA_VRAY_VRAY_ROOT/lib/." \;
+
+# Script to set environment variables during activation
+mkdir -p $PREFIX/etc/conda/activate.d
+cat <<EOF > $PREFIX/etc/conda/activate.d/$PKG_NAME-$PKG_VERSION-vars.sh
+export "MAYA_MODULE_PATH=\$CONDA_PREFIX/$MAYA_VRAY_ROOT/maya_root/modules"
+export "VRAY_EULA=https://docs.chaos.com/display/VNS/End+User+License+Agreement"
+EOF
+
+mkdir -p $PREFIX/etc/conda/deactivate.d
+cat <<EOF > $PREFIX/etc/conda/deactivate.d/$PKG_NAME-$PKG_VERSION-vars.sh
+if ! [ -z \$VRAY ]; then 
+    unset MAYA_MODULE_PATH
+    unset VRAY_EULA
+fi
+EOF
