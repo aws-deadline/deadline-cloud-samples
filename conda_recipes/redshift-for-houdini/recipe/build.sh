f601ea8316b0508bece935b19d21dca3ae8ff4f2
Michael Yuan <160265179+yuanmich2@users.noreply.github.com>
2025-03-24 13:03:01 -0700

    fix: fixed broken dynamic linking on GPU instances
    Signed-off-by: Michael Yuan <160265179+yuanmich2@users.noreply.github.com>

--- conda_recipes/redshift-for-houdini/recipe/build.sh
+++ conda_recipes/redshift-for-houdini/recipe/build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # The version without the update number
 REDSHIFT_VERSION=$PKG_VERSION
-REDSHIFT_LOCATION=${PREFIX}/redshift
+REDSHIFT_LOCATION=$PREFIX/redshift
 
 # The conda-build environment is configured for packaging one pypi package into one conda package.
 # We turn off the following defaults for the below pip install.
@@ -11,7 +11,7 @@ unset PIP_NO_DEPENDENCIES
 unset PIP_IGNORE_INSTALLED
 unset PIP_NO_INDEX
 
-mv $SRC_DIR/redshift $PREFIX/
+mv $SRC_DIR/redshift $REDSHIFT_LOCATION
 
 mkdir -p "$PREFIX/etc/conda/activate.d"
 mkdir -p "$PREFIX/etc/conda/deactivate.d"
@@ -23,32 +23,31 @@ mkdir -p "$PREFIX/etc/conda/deactivate.d"
 # need to set all the environment variables needed for a custom install: https://help.maxon.net/r3d/cinema/en-us/index.html#html/Custom+Install+Locations.html#title-text
 cat > "$PREFIX/etc/conda/activate.d/$PKG_NAME-$PKG_VERSION-vars.sh" <<EOL
 export RED_VERSION=$REDSHIFT_VERSION
-export RED_LOCATION="$REDSHIFT_LOCATION"
+export RED_LOCATION=\$CONDA_PREFIX/redshift
 
-export REDSHIFT_COREDATAPATH=$REDSHIFT_LOCATION
-export REDSHIFT_LOCALDATAPATH=$REDSHIFT_LOCATION
-export REDSHIFT_PROCEDURALSPATH=$REDSHIFT_LOCATION/Procedurals
-export REDSHIFT_PREFSPATH=$REDSHIFT_LOCATION/preferences.xml
-export REDSHIFT_LICENSEPATH=$REDSHIFT_LOCATION
+export REDSHIFT_COREDATAPATH=\$RED_LOCATION
+export REDSHIFT_LOCALDATAPATH=\$CONDA_PREFIX/redshift_local_data
+export REDSHIFT_PROCEDURALSPATH=\$REDSHIFT_COREDATAPATH/Procedurals
+#export REDSHIFT_PREFSPATH=\$REDSHIFT_LOCALDATAPATH/preferences.xml
 
 export HOUDINI_DSO_ERROR=2
 
 export EXACT_HOUDINI_VERSION="20.5.487"
 if [[ -z "\${HOUDINI_VERSION-}" ]]; then
 echo "Not empty???"
-	if [[ ! \$HOUDINI_VERSION =~ [0-9]+\.[0-9]+\.[0-9]+ ]]; then
-		EXACT_HOUDINI_VERSION=\$HOUDINI_VERSION
-	else
-		MAX=$(find ${REDSHIFT_LOCATION} -maxdepth 1 -name '\$HOUDINI_VERSION\.[1-9]*' |
-			sed 's/\$HOUDINI_VERSION\.\([0-9]*\)/\1/' |
-			sort -n |
-			tail -n 1)
-		EXACT_HOUDINI_VERSION="\$HOUDINI_VERSION.\$MAX"
-	fi
+        if [[ ! \$HOUDINI_VERSION =~ [0-9]+\.[0-9]+\.[0-9]+ ]]; then
+                EXACT_HOUDINI_VERSION=\$HOUDINI_VERSION
+        else
+                MAX=\$(find \$RED_LOCATION -maxdepth 1 -name '\$HOUDINI_VERSION\.[1-9]*' |
+                        sed 's/\$HOUDINI_VERSION\.\([0-9]*\)/\1/' |
+                        sort -n |
+                        tail -n 1)
+                EXACT_HOUDINI_VERSION="\$HOUDINI_VERSION.\$MAX"
+        fi
 fi
 echo Configuring Redshift for Houdini \$EXACT_HOUDINI_VERSION
 
-export HOUDINI_PATH="$REDSHIFT_LOCATION/redshift4houdini/\${EXACT_HOUDINI_VERSION};\${HOUDINI_PATH-&}"
+export HOUDINI_PATH="\$RED_LOCATION/redshift4houdini/\${EXACT_HOUDINI_VERSION};\${HOUDINI_PATH-&}"
 EOL
 cat "$PREFIX/etc/conda/activate.d/$PKG_NAME-$PKG_VERSION-vars.sh"
 
@@ -60,11 +59,9 @@ unset REDSHIFT_COREDATAPATH
 unset REDSHIFT_LOCALDATAPATH
 unset REDSHIFT_PROCEDURALSPATH
 unset REDSHIFT_PREFSPATH
-unset REDSHIFT_LICENSEPATH
 
 unset HOUDINI_DSO_ERROR
 export HOUDINI_PATH="\${HOUDINI_PATH/$REDSHIFT_LOCATION/redshift4houdini/\${EXACT_HOUDINI_VERSION};/}"
 unset EXACT_HOUDINI_VERSION
 EOL
 cat "$PREFIX/etc/conda/deactivate.d/$PKG_NAME-$PKG_VERSION-vars.sh"
-
