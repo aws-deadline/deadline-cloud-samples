eeabfb575c8c202e1582dc4d76f777a1105fbd9d
Alex Tran <167144297+AlexTranAmz@users.noreply.github.com>
2025-06-13 01:05:57 -0700

    feat: Add VRay for Maya 2025 verison 7.10.00
    Signed-off-by: Alex Tran <167144297+AlexTranAmz@users.noreply.github.com>

--- conda_recipes/maya-vray-2025/recipe/build.sh
+++ conda_recipes/maya-vray-2025/recipe/build.sh
@@ -3,62 +3,62 @@ set -xeuo pipefail
 
 MAYA_VERSION=2025
 
-# Run the installer extracting to a temporary directory
-ls -l $SRC_DIR
-chmod u+x $SRC_DIR/vray*
-mkdir "$SRC_DIR/extracted"
-$SRC_DIR/vray* -unpackInstall $SRC_DIR/extracted
-
-cd $PREFIX
-
-# Copy the vray relocatable (aka portable) installation from the temporary directory to the prefix
-mkdir -p "$PREFIX/usr/autodesk"
-MAYA_VRAY_ROOT="usr/autodesk/maya-vray-$MAYA_VERSION"
-MAYA_VRAY_MODULE_PATH="$MAYA_VRAY_ROOT/maya_root/modules"
+# Where we will install V-Ray for Maya
+MAYA_VRAY_ROOT="$PREFIX/opt/chaos/maya-vray-$MAYA_VERSION"
 MAYA_VRAY_VRAY_ROOT="$MAYA_VRAY_ROOT/vray"
 MAYA_VRAY_MAYA_ROOT="$MAYA_VRAY_ROOT/maya_vray"
-cp -r "$SRC_DIR/extracted/" "$PREFIX/$MAYA_VRAY_ROOT"
+mkdir -p $MAYA_VRAY_ROOT
+cd $MAYA_VRAY_ROOT
+
+# Run the installer to extract the files
+chmod u+x $SRC_DIR/vray*
+$SRC_DIR/vray* -unpackInstall .
 
 # Remove the samples, they're not needed on the farm
-rm -rf $PREFIX/$MAYA_VRAY_VRAY_ROOT/samples
+rm -rf "$MAYA_VRAY_VRAY_ROOT/samples"
 # Remove the docs, they're not needed on the farm
-rm -rf $PREFIX/$MAYA_VRAY_VRAY_ROOT/docs
+rm -rf "$MAYA_VRAY_VRAY_ROOT/docs"
 
-# Create symlinks
-mkdir -p $PREFIX/bin
-ln -r -s $PREFIX/$MAYA_VRAY_MAYA_ROOT/bin/vray $PREFIX/bin/vray
-ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/bump2gloss $PREFIX/bin/bump2gloss
-ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/imapviewer $PREFIX/bin/imapviewer
-ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/img2tiledexr $PREFIX/bin/img2tiledexr
-ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/ply2vrmesh $PREFIX/bin/ply2vrmesh
-ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/vdenoise $PREFIX/bin/vdenoise
-ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/vraydr_check $PREFIX/bin/vraydr_check
-ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/vrimg2exr $PREFIX/bin/vrimg2exr
-ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/vrmesh_viewer $PREFIX/bin/vrmesh_viewer
-ln -r -s $PREFIX/$MAYA_VRAY_VRAY_ROOT/bin/vrstconvert $PREFIX/bin/vrstconvert
+# Add relative RPATHs for the vray executable and the plugins using patchelf. This is so we can
+# follow the recommendation of https://docs.conda.io/projects/conda-build/en/latest/resources/use-shared-libraries.html
+# to never use LD_LIBRARY_PATH in Conda environments.
+for FILE in "$MAYA_VRAY_MAYA_ROOT"/lib/*.so; do
+    patchelf --add-rpath '$ORIGIN/.' "$FILE"
+done
+for FILE in "$MAYA_VRAY_VRAY_ROOT"/lib/*.so.*; do
+    patchelf --add-rpath '$ORIGIN/.:$ORIGIN/../../lib/aux' "$FILE"
+done
+
+patchelf --add-rpath '$ORIGIN/../../vray/lib' $MAYA_VRAY_MAYA_ROOT/plug-ins/vrayformaya.so
 
 # Install dependencies not available on Deadline Cloud service-managed fleets
 mkdir -p $SRC_DIR/download
 cd $SRC_DIR/download
-dnf download --resolve -y xcb-util-image xcb-util-renderutil
+dnf download --resolve -y xcb-util-image xcb-util-renderutil xcb-util-cursor \
+    xcb-util-wm xcb-util-keysyms libxkbcommon-x11 xcb-util
 
 for rpm_file in $(realpath $SRC_DIR/download/*.rpm); do
     rpm2cpio "$rpm_file" | cpio -idm
 done
 
-# Copy .so's to the V-Ray installation
-find . -iname "*.so.*" -exec cp -P -r {} "$PREFIX/$MAYA_VRAY_VRAY_ROOT/lib/." \;
+# Copy .so's to the V-Ray installation. We place these in a separate auxiliary directory so they're clearly
+# separated from what came with V-Ray.
+mkdir -p "$MAYA_VRAY_ROOT/lib/aux"
+find . -iname "*.so.*" -exec cp -P -r {} "$MAYA_VRAY_ROOT/lib/aux/" \;
+
+for FILE in "$MAYA_VRAY_ROOT"/lib/aux/*.so.*; do
+    patchelf --add-rpath '$ORIGIN/.' "$FILE"
+done
 
 mkdir -p "$PREFIX/usr/autodesk/modules/maya/$MAYA_VERSION"
-cp $CONDA_PREFIX/$MAYA_VRAY_ROOT/maya_root/modules/VRayForMaya.module $CONDA_PREFIX/usr/autodesk/modules/maya/$MAYA_VERSION
-sed -i "s|+ VRayForMaya2025rhel8 0.9 ../../maya_vray|+ VRayForMaya2025rhel8 0.9 $CONDA_PREFIX/$MAYA_VRAY_MAYA_ROOT|" $CONDA_PREFIX/usr/autodesk/modules/maya/$MAYA_VERSION/VRayForMaya.module
-if grep -q "$CONDA_PREFIX/$MAYA_VRAY_MAYA_ROOT" "$CONDA_PREFIX/usr/autodesk/modules/maya/$MAYA_VERSION/VRayForMaya.module"; then
-    echo "Changing maya_root/VRayForMaya.module file path to $CONDA_PREFIX/$MAYA_VRAY_MAYA_ROOT succeeded"
+cp $MAYA_VRAY_ROOT/maya_root/modules/VRayForMaya.module $PREFIX/usr/autodesk/modules/maya/$MAYA_VERSION
+sed -i "s|+ VRayForMaya2025rhel8 0.9 ../../maya_vray|+ VRayForMaya2025rhel8 0.9 $MAYA_VRAY_MAYA_ROOT|" $PREFIX/usr/autodesk/modules/maya/$MAYA_VERSION/VRayForMaya.module
+if grep -q "$MAYA_VRAY_MAYA_ROOT" "$PREFIX/usr/autodesk/modules/maya/$MAYA_VERSION/VRayForMaya.module"; then
+    echo "Changing maya_root/VRayForMaya.module file path to $MAYA_VRAY_MAYA_ROOT succeeded"
 else
     echo "Failed to change maya_root/VRayForMaya.module file path "
     exit 1
 fi
-
 
 # Script to set environment variables during activation
 mkdir -p $PREFIX/etc/conda/activate.d
